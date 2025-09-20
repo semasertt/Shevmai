71cc4733647fbed44f3a55b6fb38139016b99dd5
Şevval Pamuk <130461759+sevvalpamuk@users.noreply.github.com>
2025-09-20 18:03:12 +0300

    ekleme

--- app/categories/category.tsx
+++ app/categories/category.tsx
@@ -1,6 +1,8 @@
 import { useLocalSearchParams } from "expo-router";
 import {View, Text, StyleSheet, FlatList, Image} from "react-native";
 import {commonStyles} from "@/src/styles/common";
+import {VaccineScheduleView} from "@/components/VaccineScheduleView";
+import {AttackPeriodsView} from "@/components/AttackPeriodsView";
 
 export default function CategoryPage() {
     const { category, records } = useLocalSearchParams<{ category: string; records?: string }>();
@@ -12,7 +14,14 @@ export default function CategoryPage() {
     } catch (e) {
         console.error("JSON parse error:", e);
     }
+// 💉 Aşı veya ⚡ Atak Dönemleri özel davranış
+    if (category === "💉 Aşı") {
+        return <VaccineScheduleView/>;
+    }
 
+    if (category === "⚡ Atak Dönemleri") {
+                return <AttackPeriodsView />;
+            }
     return (
         <View style={commonStyles.page}>
             <Text style={commonStyles.title}>{category}</Text>
